731c51f21862f2e7299fa2c5d31dabed1a678856
Alexey Dmitriev <riadwaw@gmail.com>
2011-12-20 04:53:58 +0400

    add test with size=count

--- test.cpp
+++ test.cpp
@@ -8,17 +8,21 @@ int main() {
     std::map<int, std::vector<int> > a;
     a[0].push_back(1);
     a[0].push_back(2);
+    a[42].assign(5, 0);
     a[1].push_back(3);
     a[1].pop_back();
     DEBUG(a);
+    DEBUG(a, 100);
+    DEBUG(a, 1);
+    DEBUG(a, 3);
     std::vector<char> aa;
     aa.push_back('a');
     aa.push_back('b');
     DEBUG(aa);
     DEBUG(2 * 2);
     DEBUG("blah blah");
-    int b[5]={2,3,7,13,42};
-    DEBUG(b,5);
-    DEBUG(b,3);
+    int b[5]={2, 3, 7, 13, 42};
+    DEBUG(b, 5);
+    DEBUG(b, 3);
     return 0;
 }
